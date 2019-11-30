82b32c043a988ce35203180dc079a7dc624a7dfd
Sahil Fruitwala <sahilfruitwala1997@gmail.com>
2019-11-30 15:18:06 -0800

    Settings and Payment Module

---
+++ app/src/main/java/mobile/computing/expressstore/Config/Config.java
@@ -0,0 +1,10 @@
+package mobile.computing.expressstore.Config;
+
+// For PaymentActivity Method 1
+
+
+public class Config {
+
+    public static final String PAYPAL_CLIENT_ID = "AbXFJZ-isfs6p4VXwrXV3vGXplWDCNW0wMzL34kHP3chF7xP3pTSIeYBSBhM8i3Hr0DUdOfU1KtmSuGy";
+
+}
