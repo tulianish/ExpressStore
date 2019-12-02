0853ef2637e5e5843cbe6d9e6d4ffaa946c2d9aa
Jaspreet Singh <jaspreet.singhh2018@gmail.com>
2019-12-02 06:09:29 -0400

    BugFix-PreviousOrders + Date format change

--- app/src/main/java/mobile/computing/expressstore/ExpandableListAdapter.java
+++ app/src/main/java/mobile/computing/expressstore/ExpandableListAdapter.java
@@ -10,13 +10,20 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-/*
-ExpandableListAdapter
- */
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    /**
+     * CustomAdapter for ExpandableListView
+     * Attributes: List<String> dates - List of dates that acts as Group Header
+     *             LinkedHashMap<String, List<Items>> orderDetails - contains details of each order for the customer, Strings contains the date and List contains list of items bought on that date
+     *
+     */
 
     private Context context;
     private List<String> dates;
@@ -68,10 +75,15 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         return false;
     }
 
+    /*
+    Inflating the groupView
+     */
     @Override
     public View getGroupView(int i, boolean b, View convertView, ViewGroup viewGroup) {
 
         String dateOfOrder = (String) getGroup(i);
+        String[] splitDateOfOrder = dateOfOrder.split(" ");
+        String new_dt = splitDateOfOrder[0];
         View v=convertView;
         if (v == null) {
             LayoutInflater layoutInflater = (LayoutInflater) this.context.
@@ -79,17 +91,29 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             v = layoutInflater.inflate(R.layout.expandable_list_group, null);
         }
         TextView dateOfOrderHeader = v.findViewById(R.id.dateOfOrder);
-        dateOfOrderHeader.setText(dateOfOrder);
+        DateFormat old_format = new SimpleDateFormat("yyyy-MM-dd");
+        DateFormat new_format = new SimpleDateFormat("MMMM dd, yyyy");
+        try {
+            Date dt = old_format.parse(new_dt);
+            new_dt = new_format.format(dt);
+            System.out.println(new_dt+"   csadas");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        dateOfOrderHeader.setText(new_dt);
         return v;
 
     }
 
+    /*
+    Inflating Child View
+     */
     @Override
     public View getChildView(int i, int i1, boolean b, View convertView, ViewGroup viewGroup) {
 
         View v = convertView;
         Items item=(Items) getChild(i,i1);
-       // final String expandedListText = (String) getChild(i, i1);
+        // final String expandedListText = (String) getChild(i, i1);
 
         if (v == null) {
             LayoutInflater layoutInflater = (LayoutInflater) this.context
